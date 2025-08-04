9d63e5ac2bd0bb8afcd9ac349c17f5b9639caa42
ballgobbler35 <nagalurker@gmail.com>
2025-08-04 18:58:37 +0200

    Add files via upload

---
+++ 1MAIN.lua
@@ -0,0 +1,5 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ballgobbler35/Inject45/refs/heads/main/OBFUSCATEDjonasdjnowasd.lua"))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ballgobbler35/Inject45/refs/heads/main/extra(noneedtoupdatets.lua"))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ballgobbler35/Inject45/refs/heads/main/DCAOBFUSCATED.lua"))()
