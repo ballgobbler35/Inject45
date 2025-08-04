bb9341a647c7a32ef4c6c56825251888e57cba65
ballgobbler35 <nagalurker@gmail.com>
2025-08-04 12:13:04 +0200

    Delete big dihh randy.lua

--- big dihh randy.lua
+++
@@ -1,3 +0,0 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ballgobbler35/Inject45/refs/heads/main/big%20dihh%20randy.lua"))()
-wait(5)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ballgobbler35/Inject45/refs/heads/main/rapid%20fire%20obfuscated.lua"))()
