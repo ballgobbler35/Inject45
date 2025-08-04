09efee1f09c4edaf1f5eec97c4b904e0c1ad0bd6
ballgobbler35 <nagalurker@gmail.com>
2025-08-04 12:10:36 +0200

    Add files via upload

---
+++ big dihh randy.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ballgobbler35/Inject45/refs/heads/main/big%20dihh%20randy.lua"))()
+wait(5)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ballgobbler35/Inject45/refs/heads/main/rapid%20fire%20obfuscated.lua"))()
