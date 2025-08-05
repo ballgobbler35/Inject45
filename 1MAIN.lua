e8bed62670ced816b5767c6f94d29c89b5164381
ballgobbler35 <nagalurker@gmail.com>
2025-08-05 02:55:48 +0200

    Update 1MAIN.lua

--- 1MAIN.lua
+++ 1MAIN.lua
@@ -6,3 +6,6 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/ballgobbler35/Inject4
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/ballgobbler35/Inject45/refs/heads/main/lock%20on%20obfuscated.lua"))()
 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ballgobbler35/Inject45/refs/heads/main/esp%20obfuscated.lua"))()
+
+print("Loaded. One of my favorite movies is The Dictator.")
