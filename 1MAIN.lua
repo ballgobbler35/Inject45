c52a0bc984de0b6337536df26ac83ceb3fb6ea8e
ballgobbler35 <nagalurker@gmail.com>
2025-08-06 10:28:09 +0200

    Update 1MAIN.lua

--- 1MAIN.lua
+++ 1MAIN.lua
@@ -10,5 +10,12 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/ballgobbler35/Inject4
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/ballgobbler35/Inject45/refs/heads/main/prodiddyvoid%20OBFUSCATED.lua"))()
 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ballgobbler35/Inject45/refs/heads/main/speed%20OBFUSCATED.lua"))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ballgobbler35/Inject45/refs/heads/main/fly%20OBFUSCATED.lua"))()
+
 print("Loaded. One of my favorite movies is The Dictator.")
 
+print("If the next text shows up it worked")
+
+print("WorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorksWorks")
