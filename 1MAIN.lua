329fb0d6d36e5771d33f20a367a939a0bde330d2
ballgobbler35 <nagalurker@gmail.com>
2025-11-01 20:37:20 +0100

    Update 1MAIN.lua

--- 1MAIN.lua
+++ 1MAIN.lua
@@ -1,6 +1,2 @@
---[[
-This is no longer supported.
-]]
 local local_player = game:GetService("Players").LocalPlayer
 local_player:Kick("The end of an era (I'm just kidding check the discord)");
-end
